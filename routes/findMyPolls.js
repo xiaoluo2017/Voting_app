6e589dcacd73acc477e81f695499258517350049
Xiao Luo <xiao2017luo@gmail.com>
2017-10-29 12:46:27 -0700

    Initial commit

---
+++ routes/findMyPolls.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var config = require('./config');
+var ObjectId = require('mongodb').ObjectID;
+
+router.get('/', function(req, res, next) {
+  mongo.connect(config.DATABASE_URL, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection("polls");
+    collection.find({
+      _id: ObjectId(req.body._id)
+    }).toArray(function(err, documents) {
+      if (err) return console.log(err);
+      res.send(documents);
+    })
+  })
+});
+
+module.exports = router;
