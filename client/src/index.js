1e63b8a3b5709f1f9fdb001c9508e44fe6583f21
Xiao Luo <xiao2017luo@gmail.com>
2017-10-29 18:17:06 -0700

    Delete registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/_index.css';
-import registerServiceWorker from './registerServiceWorker';
 import AppWrapper from './redux/AppWrapper'; 
 
 ReactDOM.render((
     <AppWrapper />
 ), document.getElementById('root')); 
-
-registerServiceWorker();
